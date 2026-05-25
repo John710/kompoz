d30eba47628214c3c029772d55371396b4e1c4b1
Evgeniy <Johny710@yandex.ru>
2026-05-25 23:53:46 +1000

    fix: set env before require in test; use node 24

--- test/server.test.js
+++ test/server.test.js
@@ -3,6 +3,7 @@ const assert = require('node:assert');
 
 test('server module exports an express app', () => {
   process.env.COMPOSE_MOUNTS = '/tmp/test-mounts';
+  process.env.NODE_ENV = 'test';
   const app = require('../server/index.js');
   assert.strictEqual(typeof app, 'function');
 });
